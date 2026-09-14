89a7d427c86c11410fd218e8409e469109184164
sriimohit008 <sriimohit008@gmail.com>
2026-09-14 11:23:02 +0530

    Q22 — Profit or Loss Percentage

---
+++ Day22.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, percent;
+
+    scanf("%f %f", &cp, &sp);
+
+    if(sp > cp)
+    {
+        percent = ((sp - cp) / cp) * 100;
+        printf("Profit %g%%", percent);
+    }
+    else if(cp > sp)
+    {
+        percent = ((cp - sp) / cp) * 100;
+        printf("Loss %g%%", percent);
+    }
+    else
+    {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
